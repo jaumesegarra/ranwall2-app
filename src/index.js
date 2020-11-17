ddb276dfd4d82e28b566c09fb261610fc5edd39e
Jaume Segarra <xumet12@icloud.com>
2020-11-17 20:54:56 +0100

    Updated dependencies for mac silicon

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'line-awesome/css/line-awesome.min.css';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "line-awesome/css/line-awesome.min.css";
+import "./index.scss";
+import App from "./App";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-    <Provider store={store}>
-    	<App />
-    </Provider>, 
-document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
